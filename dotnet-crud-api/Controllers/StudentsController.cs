6cde3c3caf4580e8bfce162fea674d1c898abaac
ihakalanka <akalankaih19@gmail.com>
2023-01-03 14:26:01 +0530

    first commit

---
+++ dotnet-crud-api/Controllers/StudentsController.cs
@@ -0,0 +1,81 @@
+﻿using dotnet_crud_api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_crud_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        /*private StudentService _studentService;
+
+        public StudentsController()
+        {
+            _studentService = new StudentService();
+        }*/
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            /*var students = _studentService.AllStudents();*/
+            var students = AllStudents();
+            return Ok(students);
+        }
+
+        private List<Student> AllStudents()
+        {
+            var students = new List<Student>();
+
+            var student1 = new Student
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "doe@gmail.com",
+                Phone = "123-456-7890",
+                City = "New York",
+                CreatedAt = DateTime.Now
+            };
+            students.Add(student1);
+
+            var student2 = new Student
+            {
+                Id = 2,
+                FirstName = "Jane",
+                LastName = "Doe",
+                Email = "ddddd",
+                Phone = "123-456-7890",
+                City = "New York",
+                CreatedAt = DateTime.Now
+            };
+            students.Add(student2);
+
+            var student3 = new Student
+            {
+                Id = 3,
+                FirstName = "Jane",
+                LastName = "Doe",
+                Email = "ddddd",
+                Phone = "123-456-7890",
+                City = "New York",
+                CreatedAt = DateTime.Now
+            };
+            students.Add(student3);
+
+            var student4 = new Student
+            {
+                Id = 4,
+                FirstName = "Jane",
+                LastName = "Doe",
+                Email = "ddddd",
+                Phone = "123-456-7890",
+                City = "New York",
+                CreatedAt = DateTime.Now
+            };
+            students.Add(student4);
+
+            return students;
+        }
+    }
+}
---
+++ dotnet-crud-api/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace dotnet_crud_api.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
